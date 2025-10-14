686fd9f526b3b521c88e0db4c92e695ba578e918
TomasRivarola451 <tomasrivarola451@gmail.com>
2025-10-14 14:35:15 -0300

    catalogo actualizado 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,10 +28,9 @@ export default function Hero() {
 
         {/* Botón */}
           <a
-            href="https://docs.google.com/spreadsheets/d/2PACX-1vR4OsHMq6exsvF5XFmQ2PBiU6U3pNv44Dc_F2d3MWPCXqlmLBgCecim88XZ1jSYpA/edit?usp=sharing"
+            href="https://docs.google.com/spreadsheets/d/e/2PACX-1vR4OsHMq6exsvF5XFmQ2PBiU6U3pNv44Dc_F2d3MWPCXqlmLBgCecim88XZ1jSYpA/pubhtml"
             target="_blank"
             rel="noopener noreferrer"
-            className="hero-btn s-3"
           >
           Ver Catálogo Completo 
         </a>
