4df495997d602fe4b06cf117bcb84096b2cc49b1
TomasRivarola451 <tomasrivarola451@gmail.com>
2025-12-09 11:20:04 -0300

    actualizacion numero celular

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,20 +8,21 @@ export default function Footer() {
   return (
     <footer className="footer">
       {/* Contacto principal */}
-      <div className="footer-contact">
-        <h2>¿Tenés dudas o querés hacer un pedido?</h2>
-        <p>Comunicate con nosotros por WhatsApp o redes sociales</p>
+<div className="footer-contact">
+  <h2>¿Tenés dudas o querés hacer un pedido?</h2>
+  <p>Comunicate con nosotros por WhatsApp o redes sociales</p>
 
-        <a
-          href="https://wa.me/2616851478?text=%C2%A1Hola!%20Estoy%20interesado%20en%20los%20productos%20de%20Import%20Now.%20%C2%BFPodr%C3%ADan%20brindarme%20m%C3%A1s%20informaci%C3%B3n%3F"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="whatsapp-btn"
-        >
-          <FaWhatsapp size={24} />
-          Escribir por WhatsApp
-        </a>
-      </div>
+  <a
+    href="https://wa.me/2613742906?text=%C2%A1Hola!%20Estoy%20interesado%20en%20los%20productos%20de%20Import%20Now.%20%C2%BFPodr%C3%ADan%20brindarme%20m%C3%A1s%20informaci%C3%B3n%3F"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="whatsapp-btn"
+  >
+    <FaWhatsapp size={24} />
+    Escribir por WhatsApp
+  </a>
+</div>
+
 
       <div className="footer-info">
         <div className="footer-item">
